1df0408544ca5d5ce6542c7499b1d017b66ffa6e
Acho Arnold <a.ewin@geophy.com>
2020-08-24 20:38:08 +0200

    Created new logo, favicon and app drawer

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -22,22 +22,55 @@ const useStyles = makeStyles((theme: Theme) =>
                 marginTop: -5,
             },
         },
+
+        containerSmall: {
+            ...theme.typography.h5,
+            width: 140,
+            textAlign: 'center',
+            color: theme.palette.primary.contrastText,
+            shadow: theme.palette.warning.main,
+        },
+
+        suffixSmall: {
+            '&::after': {
+                content: '""',
+                width: '80%',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                borderBottom: `2px solid ${theme.palette.warning.light}`,
+                display: 'block',
+                marginTop: -5,
+            },
+        },
     }),
 );
 
 type LogoProps = {
     backgroundColor: string;
+    variant?: 'small' | 'large';
 };
 
 export default function Logo(props: LogoProps) {
     const classes = useStyles();
     return (
-        <Box className={classes.container}>
-            <span className={classes.suffix}>
+        <Box
+            className={
+                props.variant == 'small'
+                    ? classes.containerSmall
+                    : classes.container
+            }
+        >
+            <span
+                className={
+                    props.variant == 'small'
+                        ? classes.suffixSmall
+                        : classes.suffix
+                }
+            >
                 ov-chi
                 <span
                     style={{
-                        textShadow: `3px 0 ${props.backgroundColor}, -3px 0px ${props.backgroundColor}`,
+                        textShadow: `2px 0 ${props.backgroundColor}, -2px 0px ${props.backgroundColor}`,
                     }}
                 >
                     p
--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,45 +1,75 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import LogoutIcon from '@material-ui/icons/ExitToApp';
+import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
+import Logo from '../../components/Logo';
+import useTheme from '@material-ui/core/styles/useTheme';
+import { Box } from '@material-ui/core';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
+            display: 'flex',
+        },
+        appBar: {
+            backgroundColor: theme.palette.primary.dark,
+            zIndex: theme.zIndex.drawer + 1,
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        drawerContainer: {
+            overflow: 'auto',
+        },
+        content: {
             flexGrow: 1,
+            padding: theme.spacing(3),
         },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
+
         title: {
+            fontSize: 20,
+        },
+
+        appBarLogo: {
             flexGrow: 1,
         },
     }),
 );
 
-export default function ButtonAppBar() {
+export default function ClippedDrawer() {
     const classes = useStyles();
+    const theme = useTheme();
 
     return (
         <div className={classes.root}>
-            <AppBar position="static">
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
                 <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="menu"
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        News
-                    </Typography>
+                    <Box className={classes.appBarLogo}>
+                        <Logo
+                            variant="small"
+                            backgroundColor={theme.palette.primary.dark}
+                        />
+                    </Box>
                     <Tooltip title="Logout">
                         <IconButton color="inherit" aria-label="logout">
                             <LogoutIcon />
@@ -47,6 +77,84 @@ export default function ButtonAppBar() {
                     </Tooltip>
                 </Toolbar>
             </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <Toolbar />
+                <div className={classes.drawerContainer}>
+                    <List>
+                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map(
+                            (text, index) => (
+                                <ListItem button key={text}>
+                                    <ListItemIcon>
+                                        {index % 2 === 0 ? (
+                                            <InboxIcon />
+                                        ) : (
+                                            <MailIcon />
+                                        )}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItem>
+                            ),
+                        )}
+                    </List>
+                    <Divider />
+                    <List>
+                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                            <ListItem button key={text}>
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? (
+                                        <InboxIcon />
+                                    ) : (
+                                        <MailIcon />
+                                    )}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </div>
+            </Drawer>
+            <main className={classes.content}>
+                <Toolbar />
+                <Typography paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Rhoncus dolor purus non enim praesent elementum
+                    facilisis leo vel. Risus at ultrices mi tempus imperdiet.
+                    Semper risus in hendrerit gravida rutrum quisque non tellus.
+                    Convallis convallis tellus id interdum velit laoreet id
+                    donec ultrices. Odio morbi quis commodo odio aenean sed
+                    adipiscing. Amet nisl suscipit adipiscing bibendum est
+                    ultricies integer quis. Cursus euismod quis viverra nibh
+                    cras. Metus vulputate eu scelerisque felis imperdiet proin
+                    fermentum leo. Mauris commodo quis imperdiet massa
+                    tincidunt. Cras tincidunt lobortis feugiat vivamus at augue.
+                    At augue eget arcu dictum varius duis at consectetur lorem.
+                    Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                    sapien faucibus et molestie ac.
+                </Typography>
+                <Typography paragraph>
+                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla
+                    est ullamcorper eget nulla facilisi etiam dignissim diam.
+                    Pulvinar elementum integer enim neque volutpat ac tincidunt.
+                    Ornare suspendisse sed nisi lacus sed viverra tellus. Purus
+                    sit amet volutpat consequat mauris. Elementum eu facilisis
+                    sed odio morbi. Euismod lacinia at quis risus sed vulputate
+                    odio. Morbi tincidunt ornare massa eget egestas purus
+                    viverra accumsan in. In hendrerit gravida rutrum quisque non
+                    tellus orci ac. Pellentesque nec nam aliquam sem et tortor.
+                    Habitant morbi tristique senectus et. Adipiscing elit duis
+                    tristique sollicitudin nibh sit. Ornare aenean euismod
+                    elementum nisi quis eleifend. Commodo viverra maecenas
+                    accumsan lacus vel facilisis. Nulla posuere sollicitudin
+                    aliquam ultrices sagittis orci a.
+                </Typography>
+            </main>
         </div>
     );
 }
